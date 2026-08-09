d7883f1b2b0e4618270f580c0c1d87b3e1705045
Fidan Axundova <Fidanaxundova039@gmail.com>
2026-08-09 09:44:53 +0400

    initial commit

--- bank-atm.py
+++ bank-atm.py
@@ -6,15 +6,17 @@ choice = int(input("enter your choice: "))
 if choice == 1:
     print("balance: ", balance)
 elif choice == 2:
-    if balance > 0:
-        deposit = int(input("enter your money: "))
+        deposit = int(input("enter your deposit: "))
         balance += deposit
+        print("balance: ", balance)
+
+elif choice == 3:
+    if balance>0:
+        money = int(input("enter your money: "))
+        balance -= money
         print("balance: ", balance)
     elif balance <= 0:
         print("there isn't enough money in your account")
-elif choice == 3:
-    money = int(input("enter your deposit: "))
-    balance -= money
-    print("balance: ", balance)
 
 print("total balance: ", balance)
+
